75a157af8b7bac9fc5509ffc986fc45827637305
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-03-15 20:11:42 +0600

    added navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 const Header = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto mt-6">
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
@@ -26,50 +26,40 @@ const Header = () => {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a>Item 1</a>
+                <a>Home</a>
               </li>
               <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <a>Recipies</a>
               </li>
               <li>
-                <a>Item 3</a>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Search</a>
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-3xl">Recipe Calories</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+          <ul className="menu menu-horizontal px-1 text-[#150B2BB2] text-[16px] font-normal">
             <li>
-              <a>Item 1</a>
+              <a>Home</a>
+            </li>
+            <li><a>Recipies</a> </li>
+            <li>
+              <a>About</a>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
+              <a>Search</a>
             </li>
           </ul>
         </div>
         <div className="navbar-end">
+            
+        </div>
+        <div className="navbar-end">
+          <input type="text" placeholder="Search" className="bg-[#F3F3F4] p-2 rounded-[50px] mr-2"/>
           <a className="btn">Button</a>
         </div>
       </div>
